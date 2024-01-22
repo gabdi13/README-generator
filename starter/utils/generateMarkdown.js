331e22b34a9c8a109899d0af7ff7538c5b6e0bfc
Guled <guled@ugbads-MacBook-Pro.local>
2024-01-22 23:47:46 +0000

    generate markdown function developed

--- starter/utils/generateMarkdown.js
+++ starter/utils/generateMarkdown.js
@@ -1,8 +1,44 @@
-// function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+// generateMarkdown.js
 
-`;
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+function generateMarkdown(data) {
+  const title = capitalizeFirstLetter(data.title);
+  const description = capitalizeFirstLetter(data.description);
+  const installation = capitalizeFirstLetter(data.installation);
+  const usage = capitalizeFirstLetter(data.usage);
+  const contributing = capitalizeFirstLetter(data.contributing);
+  const tests = capitalizeFirstLetter(data.tests);
+
+  return `
+# ${title}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+
+## Description
+${description}
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## Contributing
+${contributing}
+
+## Tests
+${tests}
+  `;
+}
+
+
 module.exports = generateMarkdown;
+
